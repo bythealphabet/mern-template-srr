aafe820dc048c80182fb285d256f13875333f3d9
bythealphabet <bythealphabet@gmail.com>
2020-12-21 22:46:39 -0400

    changing port

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 const config = {
   env: process.env.NODE_ENV || "development",
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 8000,
   jwtSecret: process.env.JWT_SECRET || "your_secret_key",
   sendGridKey: process.env.SENDMAIL_API_KEY || "your_sendGrid_key",
   mongoUri:
